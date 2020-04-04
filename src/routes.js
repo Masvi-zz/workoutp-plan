6055154316513a8e8998bf15e48519133eae1dbc
masvi <jeanmasvi@gmail.com>
2020-04-04 19:00:42 -0300

    user model and migration

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,17 @@
-import Router from 'express';
+import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'hello server!' }));
+routes.get('/', async (req, res) => {
+    const user = await User.create({
+        name: 'Jean Masvi',
+        email: 'jean@gmail.com',
+        phone: '5588888888',
+        password_hash: '123213445',
+    });
+
+    return res.json(user);
+});
 
 export default routes;
