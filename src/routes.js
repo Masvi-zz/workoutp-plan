538c89d61a0a9f2a7d0b2661bb0824c8de51bea9
masvi <jeanmasvi@gmail.com>
2020-04-05 22:40:14 -0300

    user controller - error

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-    const user = await User.create({
-        name: 'Jean Masvi',
-        email: 'jean@gmail.com',
-        phone: '5588888888',
-        password_hash: '123213445',
-    });
-
-    return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
